1f0c662b21f37980205e67a8ade9f455947d7d60
Leo Vivier <zaephon@gmail.com>
2020-04-16 13:52:18 +0200

    Open file after the capture process is complete
    A user is likely to want to edit the note further once the initial capture
    has been completed, for instance to run ‘org-noter’.

--- org-roam-bibtex.el
+++ org-roam-bibtex.el
@@ -409,6 +409,7 @@ notes project before calling any org-roam functions."
                     (org-roam-capture--info (list (cons 'title title)
                                                   (cons 'ref citekey-formatted)
                                                   (cons 'slug (org-roam--title-to-slug citekey)))))
+                (add-hook 'org-capture-after-finalize-hook #'org-roam-capture--find-file-h)
                 (org-roam--capture))
             (org-roam-find-file title))
         (message "Something went wrong. Check the *Warnings* buffer."))
