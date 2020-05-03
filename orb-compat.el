20daa808701747e699b50dc2d85040cc3136df5f
Mykhailo Shevchuk <mail@mshevchuk.com>
2020-05-03 16:38:06 +0200

    bump version v0.2.1

--- orb-compat.el
+++ orb-compat.el
@@ -7,7 +7,7 @@
 ;; 	Mykhailo Shevchuk <mail@mshevchuk.com>
 ;; URL: https://github.com/zaeph/org-roam-bibtex
 ;; Keywords: org-mode, roam, convenience, bibtex, helm-bibtex, ivy-bibtex, org-ref
-;; Version: 0.2
+;; Version: 0.2.1
 
 ;; This file is NOT part of GNU Emacs.
 
