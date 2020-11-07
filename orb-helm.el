dcc4430b074d6c66281fd572565b485edfc06d9c
Mykhailo Shevchuk <mail@mshevchuk.com>
2020-11-07 21:00:21 +0100

    (int) refactoring
    - merge orb-note-actions.el into org-roam-bibtex.el
    - isolate helm- and ivy-dependent stuff into separate files
    - rename several utility functions
    - move a few functions around

---
+++ orb-helm.el
@@ -0,0 +1,101 @@
+;;; orb-helm.el --- ORB support form Helm -*- lexical-binding: t -*-
+
+;; Copyright © 2020 Mykhailo Shevchuk <mail@mshevchuk.com>
+
+;; Author: Mykhailo Shevchuk <mail@mshevchuk.com>
+;; URL: https://github.com/org-roam/org-roam-bibtex
+
+;; Soft dependencies: projectile, persp-mode, helm, ivy, hydra
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+;; ============================================================================
+;;;; Dependencies
+;; ============================================================================
+
+(require 'orb-utils)
+
+(require 'helm-bibtex)
+(require 'helm-source)
+
+(declare-function org-ref-format-entry "ext:org-ref-bibtex" (key))
+
+(declare-function orb-insert-edit-notes "org-roam-bibtex" (citekey))
+
+(defvar orb-note-actions-default)
+(defvar orb-note-actions-extra)
+(defvar orb-note-actions-user)
+
+;; ============================================================================
+;;;; Note actions
+;; ============================================================================
+
+(orb-note-actions-defun helm
+  (helm :sources
+        `(((name . ,name)
+           (candidates . ,candidates)
+           (action . (lambda (f)
+                       (funcall f (list ,citekey))))))))
+
+;; ============================================================================
+;;;; Orb insert
+;; ============================================================================
+
+(defvar helm-source-orb-insert
+  (helm-build-sync-source "BibTeX entries"
+    :header-name (lambda (name)
+                   (format "%s: " name))
+    :candidates 'helm-bibtex-candidates
+    :filtered-candidate-transformer 'helm-bibtex-candidates-formatter
+    :action (helm-make-actions
+             "Edit note & insert a link"  'helm-orb-insert-edit-notes
+             "Open PDF, URL or DOI"       'helm-bibtex-open-any
+             "Open URL or DOI in browser" 'helm-bibtex-open-url-or-doi
+             "Insert citation"            'helm-bibtex-insert-citation
+             "Insert reference"           'helm-bibtex-insert-reference
+             "Insert BibTeX key"          'helm-bibtex-insert-key
+             "Insert BibTeX entry"        'helm-bibtex-insert-bibtex
+             "Attach PDF to email"        'helm-bibtex-add-PDF-attachment
+             "Show entry"                 'helm-bibtex-show-entry
+             "Add PDF to library"     'helm-bibtex-add-pdf-to-library))
+  "Helm source to use with `orb-insert'.
+A copy of `helm-source-bibtex', in which \"Edit notes\" is made
+the first (default) action.  This action calls `helm-orb-insert-edit-notes'.
+Only relevant when `orb-insert-frontend' is `helm-bibtex'.")
+
+(helm-bibtex-helmify-action orb-insert-edit-notes helm-orb-insert-edit-notes)
+
+(defun orb-helm-insert (&optional clear-cache)
+  "Run `helm-bibtex'.
+If optional CLEAR-CACHE is non-nil, re-create `bibtex-completion-cache'.
+
+This is a simple wrapper to be run from `orb-insert'."
+  (let ((helm-source-bibtex helm-source-orb-insert))
+    (helm-bibtex clear-cache)))
+
+(provide 'orb-helm)
+
+;;; orb-helm.el ends here
+;; Local Variables:
+;; coding: utf-8
+;; fill-column: 79
+;; End:
---
+++ orb-ivy.el
@@ -0,0 +1,100 @@
+;;; orb-ivy.el --- ORB support for Ivy -*- lexical-binding: t -*-
+
+;; Copyright © 2020 Mykhailo Shevchuk <mail@mshevchuk.com>
+
+;; Author: Mykhailo Shevchuk <mail@mshevchuk.com>
+;; URL: https://github.com/org-roam/org-roam-bibtex
+
+;; Soft dependencies: projectile, persp-mode, helm, ivy, hydra
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+;; ============================================================================
+;;;; Dependencies
+;; ============================================================================
+
+(require 'orb-utils)
+
+(require 'ivy-bibtex)
+
+(declare-function org-ref-format-entry "ext:org-ref-bibtex" (key))
+
+(declare-function orb-insert-edit-notes "org-roam-bibtex" (citekey))
+
+(defvar orb-note-actions-default)
+(defvar orb-note-actions-extra)
+(defvar orb-note-actions-user)
+
+;; ============================================================================
+;;;; Note actions
+;; ============================================================================
+
+(orb-note-actions-defun ivy
+  (ivy-read name
+            candidates
+            :require-match t
+            :caller 'orb-note-actions-ivy
+            :action (lambda (c)
+                      (funcall (cdr c) (list citekey)))))
+
+;; ============================================================================
+;;;; Orb insert
+;; ============================================================================
+
+(defvar orb-insert--ivy-actions
+  '(("e" ivy-orb-insert-edit-notes "Edit note & insert a link")
+    ("p" ivy-bibtex-open-pdf "Open PDF file (if present)")
+    ("u" ivy-bibtex-open-url-or-doi "Open URL or DOI in browser")
+    ("c" ivy-bibtex-insert-citation "Insert citation")
+    ("r" ivy-bibtex-insert-reference "Insert reference")
+    ("k" ivy-bibtex-insert-key "Insert BibTeX key")
+    ("b" ivy-bibtex-insert-bibtex "Insert BibTeX entry")
+    ("a" ivy-bibtex-add-PDF-attachment "Attach PDF to email")
+    ("s" ivy-bibtex-show-entry "Show entry")
+    ("l" ivy-bibtex-add-pdf-to-library "Add PDF to library")
+    ("f" (lambda (_candidate) (ivy-bibtex-fallback ivy-text)) "Fallback options"))
+  "Ivy actions to use with `orb-insert'.
+A copy of Ivy-bibtex's alist defining Ivy actions, in which
+\"Edit note & insert a link\" is made first (default) action.
+This action calls `orb-insert-edit-notes'.  Only relevant when
+`orb-insert-frontend' is `ivy-bibtex'.")
+
+(ivy-bibtex-ivify-action orb-insert-edit-notes ivy-orb-insert-edit-notes)
+
+(defun orb-ivy-insert (&optional clear-cache)
+  "Run `ivy-bibtex'.
+If optional CLEAR-CACHE is non-nil, re-create `bibtex-completion-cache'.
+
+This is a simple wrapper to be run from `orb-insert'."
+  (let* ((ivy-actions (copy-tree ivy--actions-list))
+         (ivy--actions-list
+          (plist-put ivy-actions 'ivy-bibtex orb-insert--ivy-actions))
+         (ivy-bibtex-default-action 'ivy-orb-insert-edit-notes))
+    (ivy-bibtex clear-cache)))
+
+(provide 'orb-ivy)
+
+;;; orb-ivy.el ends here
+;; Local Variables:
+;; coding: utf-8
+;; fill-column: 79
+;; End:
