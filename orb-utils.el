8796ffd272dc5fce78fea4a30dd95a345b23b296
Mykhailo Shevchuk <mail@mshevchuk.com>
2021-01-21 09:35:18 +0100

    (fix) uninitialized orb-notes-cache
    fix #159

--- orb-utils.el
+++ orb-utils.el
@@ -249,6 +249,8 @@ the value of `orb--temp-dir'."
 (defun orb-find-note-file (citekey)
   "Find note file associated with CITEKEY.
 Returns the path to the note file, or nil if it doesn’t exist."
+  (unless orb-notes-cache
+    (orb-make-notes-cache))
   (gethash citekey orb-notes-cache))
 
 (defun orb-get-buffer-keyword (keyword &optional buffer)
